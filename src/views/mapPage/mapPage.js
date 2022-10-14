53f4e5e1bb6a30ee4460b89d7effcfb0b8b7343d
tima1617 <maeltima@gmail.com>
2022-10-14 15:58:58 +0200

    add elements

--- src/views/mapPage/mapPage.js
+++ src/views/mapPage/mapPage.js
@@ -1,4 +1,8 @@
 import "./mapPage.css";
+import Adjust from "../../assets/icons/adjust.png";
+import Expand from "../../assets/icons/expand.png";
+import Eye from "../../assets/icons/eye.png";
+import Off from "../../assets/icons/on-off-button.png";
 
 function MapPage() {
   return (
@@ -6,18 +10,34 @@ function MapPage() {
       <div className="Navbar">
         <ul>
           <li>
-            <a href="">Page d'accueil</a>
+            <a href="" className="Start-simulation">
+              Start simulation
+            </a>
           </li>
           <li>
-            <a href="">Page d'accueil</a>
-          </li>
-          <li>
-            <a href="">Page d'accueil</a>
-          </li>
-          <li>
-            <a href="">Page d'accueil</a>
+            <a href="" className="Data-analytics">
+              Data Analytics
+            </a>
           </li>
         </ul>
+        <div className="footerIcon-Container">
+          <div className="Icon">
+            {" "}
+            <img className="Icon-img" src={Adjust} alt="Adjust" />
+          </div>
+          <div className="Icon">
+            {" "}
+            <img className="Icon-img" src={Expand} alt="Adjust" />
+          </div>
+          <div className="Icon">
+            {" "}
+            <img className="Icon-img" src={Eye} alt="Adjust" />
+          </div>
+          <div className="Icon">
+            {" "}
+            <img className="Icon-img" src={Off} alt="Adjust" />
+          </div>
+        </div>
       </div>
       <div className="Map-Container">
         <div className="Map-navbar"></div>
@@ -27,14 +47,10 @@ function MapPage() {
             scrolling="no"
             marginHeight="0"
             marginWidth="0"
+            height="100%"
+            width="100%"
             src="https://www.openstreetmap.org/export/embed.html?bbox=-8.547363281250002%2C39.7240885773337%2C5.383300781250001%2C53.81362579235237&amp;layer=mapnik"
           ></iframe>
-          <br />
-          <small>
-            <a href="https://www.openstreetmap.org/#map=6/47.234/-1.582">
-              View Larger Map
-            </a>
-          </small>
         </div>
       </div>
     </div>
